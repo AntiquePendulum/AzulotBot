6d30e3bbf0dc173a2a5a8b8f6d2b4f42a4f8382e
AntiqueR <39259186+AntiquePendulam@users.noreply.github.com>
2020-03-17 21:48:05 +0900

    Initial commit

---
+++ AzulotBot/Batches/Azulot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleAppFramework;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace AzulotBot.Batches
+{
+    public class Azulot : ConsoleAppBase
+    {
+        private DiscordSocketClient _client;
+        private readonly IConfiguration _configuration;
+
+        private string Token => _configuration.GetValue<string>("Azulot_Token");
+
+        public Azulot(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task Execute()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig{LogLevel = Discord.LogSeverity.in});
+            _client.Log += x =>
+            {
+                Console.WriteLine($"{x.Message}, {x.Exception}");
+                return Task.CompletedTask;
+            };
+
+        }
+    }
+}
---
+++ AzulotBot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using AzulotBot.Batches;
+using ConsoleAppFramework;
+using Microsoft.Extensions.Hosting;
+
+namespace AzulotBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Host.CreateDefaultBuilder().RunConsoleAppFrameworkAsync<Azulot>(args);
+        }
+    }
+}
